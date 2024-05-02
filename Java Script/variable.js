7271731bc9db35dbbf2424653a2cca16891d58bc
Sai-krishnaa <saikrishna34326@gmail.com>
2024-05-02 17:05:55 +0530

    initial addition of code

---
+++ Java Script/variable.js
@@ -0,0 +1,17 @@
+fullname="Sai krishna";
+age=24;
+price=99.99;
+radius=14;
+a=null;
+y= undefined;
+isFollow=false;
+
+//  JavaScript is dynamically typed, which means that variables in JavaScript can hold values of any data type, and the data type of a variable can be changed at any point during the execution of a program.
+
+console.log(fullname);
+console.log(age);
+console.log(price);
+console.log(radius);
+console.log(a);
+console.log(y);
+console.log(isFollow);
