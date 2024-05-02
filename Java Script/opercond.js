52f96f3fb8668379114beca91cb4ef15f2c27bfd
Sai-krishnaa <saikrishna34326@gmail.com>
2024-05-03 00:53:50 +0530

    initial addition of code

---
+++ Java Script/opercond.js
@@ -0,0 +1,19 @@
+// operators and conditional statements
+
+// Arithmetic Operators 
+
+let a=5;
+let b=10;
+
+console.log("a=",a ,"b=",b);
+console.log("a+b=",a+b);//Addition
+console.log("a-b=",a-b);//Subtraction
+console.log("a*b=",a*b);//Multiplication
+console.log("a/b=",a/b);//Divison
+console.log("a%b=",a%b);//Moduls
+console.log("a**b=",a**b);//Exponential
+// unary operator
+a++;//increment
+b--;//decrement
+console.log("a=",a);
+console.log("b=",b);
