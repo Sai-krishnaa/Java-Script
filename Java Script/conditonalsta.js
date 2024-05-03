f04cf44e960f1cd452b03f955f74984820484188
Sai-krishnaa <saikrishna34326@gmail.com>
2024-05-03 16:26:05 +0530

    initial addition of code

--- Java Script/conditonalsta.js
+++ Java Script/conditonalsta.js
@@ -1,1 +1,38 @@
 //conditional statement
+
+//if statement
+let mode="dark";
+let color;
+
+if(mode==="dark"){
+    color="black";
+}
+
+if(mode==="light"){
+    color="white";
+}
+
+console.log(color);
+
+//ifelse statement
+
+let age=21;
+
+if(age>=18){
+    console.log("You can vote")
+} else{
+    console.log("You cannot vote")
+}
+
+
+//if-else statement
+
+let number=15;
+
+if(number>10){
+    console.log("Number is the greater than 10");
+} else if(number<10){
+    console.log("Number is less than 10");
+} else{
+    console.log("Number is equal to 10");
+}
