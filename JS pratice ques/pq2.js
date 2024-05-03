c650a70ad89912decc078d5564810e1e46d076a8
Sai-krishnaa <saikrishna34326@gmail.com>
2024-05-03 17:46:14 +0530

    initial addition of code

--- JS pratice ques/pq2.js
+++ JS pratice ques/pq2.js
@@ -1,8 +1,54 @@
-//check a number is odd or even using conditional statement
+//Get user to input a number uisng prompt("Enter a number:"). Check if the number is a multiple  of 5 or not
 
-let num=12;
-if(num%2==0){
-    console.log("Even")
+let num= prompt("Enter a number");
+
+if(num%5==0){
+    console.log(num,"is a Multiple of 5");
 } else{
-    console.log("odd")
+    console.log(num,"is not a Multiple of 5");
 }
+
+/*Write a code which can give grades to students according to their scores
+. 80-100,A
+. 70-89,B
+. 60-69,C
+. 50-59,D
+. 0-49,F
+*/
+
+let score=75;
+let grade;
+
+if(score>=90 && score<=100){
+    grade="A";
+} else if(score>=70 && score<=89){
+    grade="B";
+} else if(score>=60 && score<=69){
+    grade="C";
+} else if(score>=50 && score<=59){
+    grade="D";
+} else if(score>=0 && score<=49){
+    grade="F";
+}
+
+console.log("According to your marks your grade is :",grade);
+
+//Rewrite the above code where Get user to input a number uisng prompt("Enter your marks:"). Show the grade according to the score
+
+let marks=prompt("Enter your marks");
+let grades;
+
+if(marks>=90 && marks<=100){
+    grades="A";
+} else if(marks>=70 && marks<=89){
+    grades="B";
+} else if(marks>=60 && marks<=69){
+    grades="C";
+} else if(marks>=50 && marks<=59){
+    grades="D";
+} else if(marks>=0 && marks<=49){
+    grades="F";
+}
+
+console.log("According to your marks your grade is :",grades);
+
--- Java Script/conditonalsta.js
+++ Java Script/conditonalsta.js
@@ -16,12 +16,12 @@ console.log(color);
 
 //ifelse statement
 
-let age=21;
+let num=21;
 
-if(age>=18){
-    console.log("You can vote")
+if(num%2==0){
+    console.log("The number is even")
 } else{
-    console.log("You cannot vote")
+    console.log("The number is odd")
 }
 
 
@@ -37,3 +37,8 @@ if(number>10){
     console.log("Number is equal to 10");
 }
 
+//Ternary operator
+
+let age=17;
+let result= age>=18 ? "adult":"notadult";
+console.log(result)
