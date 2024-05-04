5201f78900dbdf4619354bc3635946d07af2a985
Sai-krishnaa <saikrishna34326@gmail.com>
2024-05-04 14:58:18 +0530

    initial addition of code

---
+++ Java Script/loops.js
@@ -0,0 +1,13 @@
+//Loops are used to execute a piece of code again and again
+
+//FOR LOOP
+
+let sum=0;
+let n=6;
+for(let i=1;i<=n;i++){
+    sum=sum+i;
+}
+console.log("Sum=",sum);
+console.log("Loop has ended");
+
+//
