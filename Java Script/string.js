4508e2ffe78f8cc10fa8ac7141186e5bfb305e79
Sai-krishnaa <saikrishna34326@gmail.com>
2024-05-13 12:45:47 +0530

    initial addition of code

---
+++ Java Script/string.js
@@ -0,0 +1,18 @@
+//strings
+ let str="java script";
+ console.log(str);
+
+ //Template Literals
+
+ let specialstring= `This is a template literal`;
+ console.log(typeof specialstring);
+
+ //example of template literals
+
+ let obj={
+    item:"Pen",
+    price: 20,
+ } 
+
+ let output=`The cost of ${obj.item} is ${obj.price} rupees`;
+ console.log(output)
