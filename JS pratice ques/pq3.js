c0bbcd1d18ee3d212e0b973c5022315cf7d179a7
Sai-krishnaa <saikrishna34326@gmail.com>
2024-05-13 11:38:13 +0530

    initial addition of code

--- JS pratice ques/pq3.js
+++ JS pratice ques/pq3.js
@@ -1,7 +1,17 @@
 //Print all even numbers from 0 to 100
 
 for(let num=0;num<=100;num++){
-    if(num%2===0){
-        console.log("Num",num);
-    } 
+    if(num %2===0){
+        console.log("Num=",num);
+    }
+}
+
+
+// Get user to input a number uisng prompt("Enter a number:"). Check if the number is even or odd
+let n= prompt("Enter a number")
+
+if(n %2===0){
+    console.log(n,"The number is even");
+} else{
+    console.log(n,"The number is not even");
 }
